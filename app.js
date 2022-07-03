e3028b43d1faab746d0d62e5c0a6fe057b9210dd
andriand06 <64359593+andriand06@users.noreply.github.com>
2022-07-03 17:39:51 +0700

    allow acces control headers

--- app.js
+++ app.js
@@ -51,6 +51,7 @@ app.use(
 );
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
   next();
 });
 app.use("/", indexRouter);
